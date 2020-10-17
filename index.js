4a03b4ddf2f00e7914c88b07ec0796131a458f97
Shamel Lakin <christlife4u@gmail.com>
2020-10-17 14:57:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+const findMatching = (drivers) => {
+  drivers.filter( drive => {
+
+ })
+}
+ 
+
+// return  drivers.filter( driver => {
+//     return (driver.split(' ').filter(word => {
+//         return word.charAt(0).toUpperCase() + word.slice(1);
+//     })).join(' ') 
+//  }) 
